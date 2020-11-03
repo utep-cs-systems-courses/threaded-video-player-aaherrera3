d460b38b0e8709c70060656d6ac9894876949966
student <aaherrera3@miners.utep.edu>
2020-11-03 10:24:28 -0700

    Producer and Consumer queues

---
+++ VideoLab/FrameQueue.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+import cv2, os, sys
+from threading import Thread, Semaphore, Lock
+
+class FrameQueue():
+    def __init__(self,cap = 10):
+        self.queue = []
+        self.qLock = Lock()
+        self.full = Semaphore(0)
+        self.empty = Semaphore(cap)
+
+    def add(self,frame):
+        self.empty.acquire()
+        self.qLock.acquire()
+        self.queue.append(frame)
+        self.qLock.release()
+        self.full.release()
+
+    def remove(self,frame):
+        self.full.acquire()
+        self.qLock.aquire()
+        return self.queue.pop(0)
+        self.qLock.release()
+        self.empty.release()
+
+
+    
